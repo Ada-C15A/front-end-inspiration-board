cc229886c10edd8f6f5e6cb70bf2a93eb792a05c
Naomi Quinones <naomiquinones@gmail.com>
2021-07-16 09:02:48 -0700

    Move functions up to parent component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Board.css';
 import CardList from './CardList.js';
@@ -7,7 +7,42 @@ import NewCardForm from './NewCardForm';
 const axios = require('axios');
 
 const Board = (props) => {
+  const [cards, setCards] = useState([]);
   const [makeNewCard, setMakeNewCard] = useState(false);
+
+  const getCards = () => {
+    axios
+    .get(`http://localhost:5000/boards/${props.board_id}/cards`)
+    .then( response => {
+      setCards(response.data);
+    })
+    .catch(error => console.log(error))
+    .finally( () => console.log(`Tried to get cards for board ${props.board_id}`))
+  }
+  const onDeleteCard = id => {
+    axios
+      .delete(`${process.env.REACT_APP_BACKEND_URL}/${id}`)
+      .then( response => {
+          const newCards= cards.filter( (card) => card.card_id !== id)
+        setCards(newCards);
+      })
+      .catch(error => console.log(error))
+      .finally( () => console.log(`Tried to get cards for board ${id}`))
+    }
+
+    const onVoteCard = (id, vote )=> {
+      axios
+      .patch(`${process.env.REACT_APP_BACKEND_URL}/${id}/votes?like_count=${vote}`)
+      .then( response => {
+        getCards();
+    })
+    .catch(error => console.log(error))
+    .finally( () => console.log(`Tried to update votes`))
+    }
+
+    useEffect( () => {
+    getCards();
+    }, [props.board_id]);
 
   const onNewCardButtonClick = event => {
     setMakeNewCard(true);
@@ -17,6 +52,7 @@ const Board = (props) => {
     axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards/${props.board_id}/cards`, message)
     .then( response => {
       console.log(response.data)
+      getCards();
     })
     .catch(error => console.log(error))
     .finally('Tried to make a new card')
@@ -25,7 +61,12 @@ const Board = (props) => {
     <section className="board">
       <h2>Title: {props.title}</h2>
       <p>Owner: {props.owner}</p>
-      <CardList board_id={props.board_id} />
+      <CardList
+        cards={cards}
+        board_id={props.board_id} 
+        onDeleteCard={onDeleteCard}
+        onVoteCard={onVoteCard}
+       />
       <button className="create-button" onClick={onNewCardButtonClick}>Create New Card</button>
 
       {makeNewCard && <NewCardForm addCardCallback={addCard} />}
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,59 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Card from './Card';
 import './CardList.css';
-import PropTypes from 'prop-types';
-
-const axios = require('axios');
+// import PropTypes from 'prop-types';
 
 const CardList = props => {
-  const [cards, setCards] = useState([]);
-  const [selectedCard, setSelectedCard] = useState({card:{board_id: null, card_id: null, message:'', like_count:''}});
-
-  const getCards = () => {
-    axios
-    .get(`http://localhost:5000/boards/${props.board_id}/cards`)
-    .then( response => {
-      setCards(response.data);
-    })
-    .catch(error => console.log(error))
-    .finally( () => console.log(`Tried to get cards for board ${props.board_id}`))
-  }
-
-  useEffect( () => {
-    getCards();
-    }, []);
-
-  const onDeleteCard = id => {
-    axios
-      .delete(`${process.env.REACT_APP_BACKEND_URL}/${id}`)
-      .then( response => {
-          const newCards= cards.filter( (card) => card.card_id !== id)
-        setCards(newCards);
-      })
-      .catch(error => console.log(error))
-      .finally( () => console.log(`Tried to get cards for board ${id}`))
-    }
-
-    const onVoteCard = (id, vote )=> {
-      axios
-      .patch(`${process.env.REACT_APP_BACKEND_URL}/${id}/votes?like_count=${vote}`)
-      .then( response => {
-        getCards();
-    })
-    .catch(error => console.log(error))
-    .finally( () => console.log(`Tried to update votes`))
-    }
+  // const [selectedCard, setSelectedCard] = useState({card:{board_id: null, card_id: null, message:'', like_count:''}});
 
   return (
     <section id="card-list">
-    {cards.map( card => 
+    {props.cards.map( card => 
       <Card key={card.card_id} 
         board_id={card.board_id} 
         card_id={card.card_id} 
         message={card.message} 
         votes={card.votes} 
-        onDeleteCard={onDeleteCard}
-        onVoteCard={onVoteCard} 
+        onDeleteCard={props.onDeleteCard}
+        onVoteCard={props.onVoteCard} 
       />
     )}
     </section>
